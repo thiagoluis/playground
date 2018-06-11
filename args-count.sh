0d37545071e5dc14fe8e6cf0b14bf147511eea4f
Thiago Luís <thiagoluismo@gmail.com>
2018-06-11 20:33:10 -0300

    Add a exit code.

--- args-count.sh
+++ args-count.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # $# - number of args
+# exit != 0 it's mean a error.
 
-[[ "$#" != "1" ]] && echo You must pass one argument! && exit
+[[ "$#" != "1" ]] && echo You must pass one argument! && exit 1
 echo You pass one argument: $1
+exit 0
